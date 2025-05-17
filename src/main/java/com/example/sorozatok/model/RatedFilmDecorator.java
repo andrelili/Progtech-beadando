2067a37dbf89fb413d8efbed5eb5e16d31722d2b
André Lili Anna <166949390+andrelili@users.noreply.github.com>
2025-05-17 16:41:12 +0200

    adatbázis és hibajavítás

--- src/main/java/com/example/sorozatok/model/RatedFilmDecorator.java
+++ src/main/java/com/example/sorozatok/model/RatedFilmDecorator.java
@@ -1,36 +1,53 @@
 package com.example.sorozatok.model;
 
-public class RatedFilmDecorator {
-    private Film baseFilm;
-    private double userRating; //A bejelentkezett felhasználó értékelése
+public class RatedFilmDecorator implements FilmComponent{
+    private final FilmComponent baseFilm;
+    private final double userRating; //A bejelentkezett felhasználó értékelése
 
     public RatedFilmDecorator(Film basefilm, double userRating) {
         this.baseFilm = basefilm;
         this.userRating = userRating;
     }
 
+    @Override
     public int getId() {
         return baseFilm.getId();
     }
 
+    @Override
     public String getTitle() {
         return baseFilm.getTitle();
     }
 
+    @Override
     public Status getStatus() {
         return baseFilm.getStatus();
     }
 
-    public Film getBaseFilm() {
+    @Override
+    public double getAverageRating() {
+        return baseFilm.getAverageRating();
+    }
+    @Override
+    public int getRatingCount() {
+        return baseFilm.getRatingCount();
+    }
+
+    public FilmComponent getBaseFilm() {
         return baseFilm;
     }
 
     public void updateFilmRating() {
-        double currentTotal = baseFilm.getAverageRating() * baseFilm.getRatingCount(); // összes eddigi értékelés
-        int newCount = baseFilm.getRatingCount() + 1;
+        if (!(baseFilm instanceof Film)) {
+            throw new IllegalStateException("Only base Film instances can be updated!");
+        }
+
+        Film film = (Film) baseFilm;
+        double currentTotal = film.getAverageRating() * film.getRatingCount(); // összes eddigi értékelés
+        int newCount = film.getRatingCount() + 1;
         double newAverage = (currentTotal + userRating) / newCount;
 
-        baseFilm.setAverageRating(newAverage);
-        baseFilm.setRatingCount(newCount);
+        film.setAverageRating(newAverage);
+        film.setRatingCount(newCount);
     }
 }
