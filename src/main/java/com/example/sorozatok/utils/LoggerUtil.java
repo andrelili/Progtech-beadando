fcdf30448c4b70590e564fd5eb2c78d542bbebd2
EBTBSD <zsola.faz2002@outlook.hu>
2025-05-18 11:09:37 +0200

    Logger és Password Util fájlok létrehozása, megírása

---
+++ src/main/java/com/example/sorozatok/utils/LoggerUtil.java
@@ -0,0 +1,25 @@
+package com.example.sorozatok.utils;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoggerUtil {
+    private static final Logger logger = Logger.getLogger("AppLogger");
+
+    static {
+        try {
+            FileHandler handler = new FileHandler("app.log", true);
+            logger.addHandler(handler);
+            logger.setLevel(Level.INFO);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void log(String msg) {
+        logger.info(msg);
+    }
+}
+
---
+++ src/main/java/com/example/sorozatok/utils/PasswordUtil.java
@@ -0,0 +1,19 @@
+package com.example.sorozatok.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class PasswordUtil {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashed = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hashed);
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+        return password;
+    }
+}
