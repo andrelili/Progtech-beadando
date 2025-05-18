de9edb51c4328cba0f08e7dd052d2f15e9979f38
EBTBSD <zsola.faz2002@outlook.hu>
2025-05-18 11:21:44 +0200

    SessionManager elkészítése

---
+++ src/main/java/com/example/sorozatok/manager/SessionManager.java
@@ -0,0 +1,26 @@
+package com.example.sorozatok.manager;
+
+import com.example.sorozatok.model.User;
+
+public class SessionManager {
+    private static SessionManager instance;
+    private User currentUser;
+
+    private SessionManager() {}
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+}
+
