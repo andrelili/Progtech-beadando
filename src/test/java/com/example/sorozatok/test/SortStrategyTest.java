ae2e9abf587c987ea1de4bc90b34932add370841
Halolol069 <144492413+Halolol069@users.noreply.github.com>
2025-05-26 22:02:19 +0200

    test kibővítése

--- src/test/java/com/example/sorozatok/test/SortStrategyTest.java
+++ src/test/java/com/example/sorozatok/test/SortStrategyTest.java
@@ -3,6 +3,8 @@ package com.example.sorozatok.test;
 import com.example.sorozatok.model.Film;
 import com.example.sorozatok.model.Genre;
 import com.example.sorozatok.model.Status;
+import com.example.sorozatok.strategy.SortByRating;
+import com.example.sorozatok.strategy.SortByTitle;
 import com.example.sorozatok.strategy.SortByYear;
 import com.example.sorozatok.strategy.SortStrategy;
 import org.junit.jupiter.api.Test;
@@ -40,4 +42,45 @@ public class SortStrategyTest {
         assertEquals(2021, sortedFilms.get(1).getYear());
         assertEquals(2019, sortedFilms.get(2).getYear());
     }
+    @Test
+    public void testSortByTitleDescending(){
+        List<Film> films = Arrays.asList(
+                new Film("Film A", Status.PLANNED, Genre.ACTION, 4.5, 100, 2021),
+                new Film("Film B", Status.WATCHED, Genre.COMEDY, 4.2,200,2019),
+                new Film("Film C", Status.DROPPED, Genre.DRAMA, 2.1, 150, 2023)
+        );
+        SortStrategy sortStrategy = new SortByTitle(false);
+        List<Film> sortedFilm = sortStrategy.sort(films);
+        assertEquals("Film C", sortedFilm.get(0).getTitle());
+        assertEquals("Film B", sortedFilm.get(1).getTitle());
+        assertEquals("Film A", sortedFilm.get(2).getTitle());
+    }
+    @Test
+    public void testSortByRatingAscending(){
+        List<Film> films = Arrays.asList(
+                new Film("Film A", Status.PLANNED, Genre.ACTION, 4.5, 100, 2021),
+                new Film("Film B", Status.WATCHED, Genre.COMEDY, 4.2,200,2019),
+                new Film("Film C", Status.DROPPED, Genre.DRAMA, 2.1, 150, 2023)
+        );
+        SortStrategy sortStrategy = new SortByRating(false);
+        List<Film> sortedFilm = sortStrategy.sort(films);
+        assertEquals(4.5, sortedFilm.get(0).getAverageRating());
+        assertEquals(4.2, sortedFilm.get(1).getAverageRating());
+        assertEquals(2.1, sortedFilm.get(2).getAverageRating());
+    }
+    @Test
+    public void testSortByRatingDescending(){
+        List<Film> films = Arrays.asList(
+                new Film("Film A", Status.PLANNED, Genre.ACTION, 4.5, 100, 2021),
+                new Film("Film B", Status.WATCHED, Genre.COMEDY, 4.2,200,2019),
+                new Film("Film C", Status.DROPPED, Genre.DRAMA, 2.1, 150, 2023)
+        );
+        SortStrategy sortStrategy = new SortByRating(true);
+        List<Film> sortedFilm = sortStrategy.sort(films);
+        assertEquals(2.1, sortedFilm.get(0).getAverageRating());
+        assertEquals(4.2, sortedFilm.get(1).getAverageRating());
+        assertEquals(4.5, sortedFilm.get(2).getAverageRating());
+    }
+
+
 }
